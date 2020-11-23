da1b2fb3ca2d8b2d0647a54a3e5a32fe97600d9f
Lev112233 <74585578+Lev112233@users.noreply.github.com>
2020-11-23 09:48:24 +0300

    Create IPlayerDataAdapter.java

---
+++ IPlayerDataAdapter.java
@@ -0,0 +1,40 @@
+package io.github.Leonardo0013YT.UltraSkills.adapters;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+
+public class IPlayerDataAdapter implements JsonSerializer, JsonDeserializer {
+  private static final String CLASSNAME = "IPlayerData";
+  
+  private static final String DATA = "Data";
+  
+  public Object deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+    JsonObject jsonObject = jsonElement.getAsJsonObject();
+    JsonPrimitive prim = (JsonPrimitive)jsonObject.get("IPlayerData");
+    String className = prim.getAsString();
+    Class<?> klass = getObjectClass(className);
+    return jsonDeserializationContext.deserialize(jsonObject.get("Data"), klass);
+  }
+  
+  public JsonElement serialize(Object jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("IPlayerData", jsonElement.getClass().getName());
+    jsonObject.add("Data", jsonSerializationContext.serialize(jsonElement));
+    return (JsonElement)jsonObject;
+  }
+  
+  public Class<?> getObjectClass(String className) {
+    try {
+      return Class.forName(className);
+    } catch (ClassNotFoundException e) {
+      throw new JsonParseException(e.getMessage());
+    } 
+  }
+}
